a4ed165990d95b7dfeb6e73f473a63b08eeb8976
Dipak Acharya <dev.dipak21@gmail.com>
2026-09-22 13:53:29 +0545

    Make server addr and port customizable via CLI args

--- pkg/client/main.go
+++ pkg/client/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"embed"
+	"flag"
 	"fmt"
 	"io/fs"
 	"log"
+	"net"
 	"ttt-game/internal/client"
 )
 
@@ -12,11 +14,18 @@ import (
 var uiFs embed.FS
 
 func main() {
+	serverHost := flag.String("server-host", "0.0.0.0", "server host")
+	serverPort := flag.String("server-port", "16000", "server port")
+
+	flag.Parse()
+
+	fmt.Println("Client will connect to the server:", net.JoinHostPort(*serverHost, *serverPort))
+
 	fs, _ := fs.Sub(uiFs, "ui")
 
 	client, err := client.New(&client.Config{
-		ServerHost: "127.0.0.1",
-		ServerPort: "16000",
+		ServerHost: *serverHost,
+		ServerPort: *serverPort,
 		ClientHost: "0.0.0.0",
 		ClientPort: "0",
 		UIFS:       fs,
@@ -27,7 +36,7 @@ func main() {
 		return
 	}
 
-	fmt.Println("URL:", client.URL())
+	fmt.Println("Play game at:", client.URL())
 
 	client.Run()
 }
--- pkg/server/main.go
+++ pkg/server/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"ttt-game/internal/server"
 )
 
 func main() {
+	port := flag.String("port", "16000", "server port")
+
+	flag.Parse()
+
+	fmt.Println("The server will start on port:", *port)
+
 	_, err := server.New(&server.Config{
 		Host: "0.0.0.0",
-		Port: "16000",
+		Port: *port,
 	})
 
 	if err != nil {
